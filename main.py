125a64b6bd492edb211a5bc7fedd02390d01f1f8
esmabulutayd <149339187+Esmabulutayd@users.noreply.github.com>
2024-04-01 23:45:52 +0100

    Program changed to define functions before implementing top-level code

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
+def plane_cost(city_flight):
+
+    if city_flight == "Istanbul":
+        return 300  # Program assumes cost of Istanbul flight is £300
+    if city_flight == "London":
+        return 500  # Program assumes cost of London flight is £500
+    if city_flight == "Amsterdam":
+        return 400  # Program assumes cost of Amsterdam flight is £400
+    else:
+        return 0
+
+def hotel_cost(num_nights):
+
+    if city_flight == "Istanbul":
+        return num_nights * 100  # Program assumes daily hotel rate is £100 in Istanbul
+    if city_flight == "London":
+        return num_nights * 250  # Program assumes daily hotel rate is £250 in London
+    if city_flight == "Amsterdam":
+        return num_nights * 200  # Program assumes daily hotel rate is £200 in Amsterdam
+    else:
+        return 0
+
+def car_rental(rental_days):
+
+    return rental_days * 75  # Program assumes a daily rental cost of £75 for the car
+
+def holiday_cost(hotel_total, plane_total, car_total):
+
+    return hotel_total + plane_total + car_total
+
+
 while True:
+
     print("Welcome to holiday budget calculator!\n")
 
     cities = ["Istanbul", "London", "Amsterdam"]
@@ -24,33 +56,7 @@ while True:
         rental_days = input("Please enter number of the days you would like to rent a car: \n")
 
     rental_days = int(rental_days)
-
-    def plane_cost(city_flight):
-        if city_flight == "Istanbul":
-            return 300  # Program assumes cost of Istanbul flight is £300
-        elif city_flight == "London":
-            return 500  # Program assumes cost of London flight is £500
-        elif city_flight == "Amsterdam":
-            return 400  # Program assumes cost of Amsterdam flight is £400
-        else:
-            return 0
-
-    def hotel_cost(num_nights):
-        if city_flight == "Istanbul":
-            return num_nights * 100  # Program assumes daily hotel rate is £100 in Istanbul
-        elif city_flight == "London":
-            return num_nights * 250  # Program assumes daily hotel rate is £250 in London
-        elif city_flight == "Amsterdam":
-            return num_nights * 200  # Program assumes daily hotel rate is £200 in Amsterdam
-        else:
-            return 0
-
-    def car_rental(rental_days):
-        return rental_days * 75  # Program assumes a daily rental cost of £75 for the car
-
-    def holiday_cost(hotel_total, plane_total, car_total):
-        return hotel_total + plane_total + car_total
-
+    
     hotel_total = hotel_cost(num_nights)
     flight_total = plane_cost(city_flight)
     car_total = car_rental(rental_days)
